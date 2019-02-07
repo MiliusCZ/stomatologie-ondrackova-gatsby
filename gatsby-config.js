56b9da58711eecc1968de7c90fb24b295ba8dc53
Milos Turek <milos.turek@gmail.com>
2019-02-07 19:59:09 +0100

    updated non-working gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,12 +36,12 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography.js',
       },
     },
-    {
+    /* {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
-    },
+    },*/
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
@@ -53,7 +53,6 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui'
-        // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import SEO from '../components/seo';
+// import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <h1>Hi people</h1>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
