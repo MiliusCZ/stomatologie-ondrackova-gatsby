8b4e174d85012c4d374f20b01f01b36b64659879
Milos Turek <milos.turek@gmail.com>
2019-02-27 22:27:31 +0100

    added header to layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,42 +1,18 @@
 import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
+import './Header.scss';
+
+const Header = () => (
+  <header>
+    <div className="header">
+      <div>
+        <Link to="/">
+          <img src="/img/logo.png" />
         </Link>
-      </h1>
+      </div>
     </div>
-  </div>
+  </header>
 );
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: '',
-};
 
 export default Header;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,13 +4,17 @@ import Image from '../components/Image';
 
 import './index.scss';
 
-//import Layout from '../components/layout';
+import Layout from '../components/layout';
 // import SEO from '../components/seo';
 
 const IndexPage = () => (
-  <div className="main">
-    <div className="logoPlaceholder"><Image /></div>
-  </div>
+  <Layout>
+    <div className="main">
+      <div className="logoPlaceholder">
+        <Image />
+      </div>
+    </div>
+  </Layout>
 );
 
 export default IndexPage;
