0dbabe9ea64d558f1d9b7f9ffbcbd8dd797b38f2
Milos Turek <milos.turek@gmail.com>
2019-03-20 18:11:16 +0100

    updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,47 +1,41 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Stomatologie Onrackova',
-    description: 'Test description',
-    author: 'Milos Turek'
-  },
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-transformer-json',
     'gatsby-transformer-remark',
-    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/configuration/site.json`,
-        name: 'config',
-      },
+        name: 'config'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/content`,
-        name: 'content',
-      },
+        name: 'content'
+      }
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/static/img`,
-        name: 'image',
-      },
+        name: 'image'
+      }
     },
     {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography.js',
-      },
+      }
     },
-    /* {
+    {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },*/
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
@@ -53,11 +47,11 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui'
-      },
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    // 'gatsby-plugin-offline'
     {
       resolve: 'gatsby-plugin-eslint',
       options: {
